7af1973654e80acf6bd9a47517c8d2797c5957ed
Andreas Klapperich <andreas@MacBook003.fritz.box>
2018-04-26 20:51:42 +0200

    Fixes of a stupid developer :)

--- Program.cs
+++ Program.cs
@@ -4,45 +4,54 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 
-namespace DotNetCode {
-    class Program {
-        static void Main (string[] args) {
-            if (args?.Length > 0 && (args[0] == "--help" || args[0] == "-h")) {
+namespace DotNetCode
+{
+    class Program
+    {
+        static bool isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+        static void Main(string[] args)
+        {
+            if (args?.Length > 0 && (args[0] == "--help" || args[0] == "-h"))
+            {
                 string Help = $"dotnet-code {System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString()} starts Visual Studio Code in the current folder.\r\n" +
                     "If there is a code-workspace file in the folder, dotnet-code starts code with this,\r\n" +
                     "otherwise with the current folder as parameter.";
-                Console.WriteLine (Help);
+                Console.WriteLine(Help);
                 return;
             }
-            var isWindows = System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform (System.Runtime.InteropServices.OSPlatform.Windows);
-            var workspace = Directory.GetFiles (Directory.GetCurrentDirectory (), "*.code-workspace").FirstOrDefault ();
 
-            ProcessStartInfo psi = new ProcessStartInfo ();
-            psi.WindowStyle = ProcessWindowStyle.Hidden;
-            psi.RedirectStandardOutput = true;
-            psi.RedirectStandardError = true;
-            psi.UseShellExecute = false;
-            psi.CreateNoWindow = true;
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string workspaceFilename = Directory.GetFiles(currentDirectory, "*.code-workspace").FirstOrDefault();
 
-            try {
-                if (string.IsNullOrEmpty (workspace)) {
-                    if (isWindows) {
-                        var p = new Process ();
-                        p.StartInfo = psi;
-                        p = Process.Start ("cmd", $"/C code {Directory.GetCurrentDirectory()}");
-                    } else
-                        Process.Start ("code", $"{Directory.GetCurrentDirectory()}");
-                } else {
-                    if (isWindows) {
-                        var p = new Process ();
-                        p.StartInfo = psi;
-                        p = Process.Start ("cmd", $"/C code {workspace}");
-                    } else
-                        Process.Start ("code", $"{workspace}");
+            StartCode(string.IsNullOrEmpty(workspaceFilename) ? currentDirectory : workspaceFilename);
+        }
+
+        static void StartCode(string parameter)
+        {
+            try
+            {
+                if (isWindows)
+                {
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = "cmd.exe",
+                        Arguments = $"/C code {parameter}",
+
+                        WindowStyle = ProcessWindowStyle.Hidden,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = false,
+                    });
                 }
-            } catch (Exception ex) {
-                Console.WriteLine ("Can no start Visual Studio Code");
-                Console.WriteLine (ex);
+                else
+                    Process.Start("code", $"{parameter}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Can no start Visual Studio Code");
+                Console.WriteLine(ex);
             }
         }
     }
